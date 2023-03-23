16e69254efd3fa1cf7b7ebee1ce08b4a6dcc869c
Saswat <saswatds@gmail.com>
2023-03-23 08:29:53 -0700

    feat: added redis module

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # cortes
 A lightweight opinionated framework for building services in Node.js
+
+## Modules
+- [x] Load
+- [x] Config
+- [x] BullMQ
+- [x] Redis
+- [ ] Cassandra
+- [ ] Logger
+- [ ] MongoDB
+- [ ] Newrelic
+- [ ] Request
+- [x] Sentry
+- [ ] Storage
---
+++ packages/redis/src/index.ts
@@ -0,0 +1,66 @@
+import type { Cb, Ctx } from '@cortes/types';
+import config from 'config';
+import type { Cluster } from 'ioredis';
+import Redis from 'ioredis';
+
+export default function (ctx: Ctx, _: unknown, done: Cb) {
+  const cacheConfig = config.util.toObject(config.get('cache')),
+    cache: { [name: string]: Cluster | Redis } = {};
+
+  Object.keys(cacheConfig).forEach((identity) => {
+    const defaultConfig = cacheConfig[identity] || {},
+      redisOptions = { ...defaultConfig.connection };
+
+    redisOptions.reconnectOnError = (err: Error) =>
+      err.message && err.message.startsWith('READONLY');
+    redisOptions.retryStrategy = (times: number) => {
+      if (times >= defaultConfig.maxRetries) {
+        return false;
+      }
+
+      // eslint-disable-next-line no-magic-numbers
+      return Math.min(times * 50, 2000);
+    };
+
+    // If encryption is enabled then
+    if (defaultConfig.encryption) {
+      redisOptions.tls = {};
+    }
+
+    // If we are not running in cluster mode then its super simple
+    if (!defaultConfig.cluster) {
+      cache[identity] = new Redis(redisOptions);
+
+      return;
+    }
+
+    // From this point onwards we are working with CLUSTERs
+    const clusterOptions = defaultConfig.cluster || {},
+      { nodes = [] } = clusterOptions;
+
+    // Delete the nodes property from options
+    delete clusterOptions.nodes;
+
+    // Add the master node to nodes list
+    nodes.unshift({
+      host: redisOptions.host,
+      port: redisOptions.port,
+    });
+
+    // We can now delete the host and port from redisOptions
+    delete redisOptions.host;
+    delete redisOptions.port;
+
+    cache[identity] = new Redis.Cluster(nodes, {
+      ...clusterOptions,
+      redisOptions,
+    });
+  });
+
+  ctx.set('cache', (name: string) => cache[name]);
+
+  return done(undefined, (next) => {
+    [...Object.values(cache)].forEach((redis) => redis.disconnect());
+    next();
+  });
+}
