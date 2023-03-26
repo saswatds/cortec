d4f7af93f59eb459735e9b81a9a507c8be32910b
Saswat <saswatds@gmail.com>
2023-03-26 10:01:50 -0700

    feat: migrate redis to a module interface

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -1,66 +1,74 @@
-import type { Cb, Ctx } from '@cortec/types';
-import config from 'config';
+import type { IContext, Module } from '@cortec/types';
 import type { Cluster } from 'ioredis';
 import Redis from 'ioredis';
 
-export default function (ctx: Ctx, _: unknown, done: Cb) {
-  const cacheConfig = config.util.toObject(config.get('cache')),
-    cache: { [name: string]: Cluster | Redis } = {};
+export default class CortecRedis implements Module {
+  name = 'redis';
+  loaded = false;
 
-  Object.keys(cacheConfig).forEach((identity) => {
-    const defaultConfig = cacheConfig[identity] || {},
-      redisOptions = { ...defaultConfig.connection };
+  private $cache: { [name: string]: Cluster | Redis } = {};
+  async load(ctx: IContext) {
+    const config = ctx.provide<any>('config');
+    const cacheConfig = config.get('cache'),
+      cache: { [name: string]: Cluster | Redis } = {};
 
-    redisOptions.reconnectOnError = (err: Error) =>
-      err.message && err.message.startsWith('READONLY');
-    redisOptions.retryStrategy = (times: number) => {
-      if (times >= defaultConfig.maxRetries) {
-        return false;
+    Object.keys(cacheConfig).forEach((identity) => {
+      const defaultConfig = cacheConfig[identity] || {},
+        redisOptions = { ...defaultConfig.connection };
+
+      redisOptions.reconnectOnError = (err: Error) =>
+        err.message && err.message.startsWith('READONLY');
+      redisOptions.retryStrategy = (times: number) => {
+        if (times >= defaultConfig.maxRetries) {
+          return false;
+        }
+
+        // eslint-disable-next-line no-magic-numbers
+        return Math.min(times * 50, 2000);
+      };
+
+      // If encryption is enabled then
+      if (defaultConfig.encryption) {
+        redisOptions.tls = {};
       }
 
-      // eslint-disable-next-line no-magic-numbers
-      return Math.min(times * 50, 2000);
-    };
+      // If we are not running in cluster mode then its super simple
+      if (!defaultConfig.cluster) {
+        cache[identity] = new Redis(redisOptions);
 
-    // If encryption is enabled then
-    if (defaultConfig.encryption) {
-      redisOptions.tls = {};
-    }
+        return;
+      }
 
-    // If we are not running in cluster mode then its super simple
-    if (!defaultConfig.cluster) {
-      cache[identity] = new Redis(redisOptions);
+      // From this point onwards we are working with CLUSTERs
+      const clusterOptions = defaultConfig.cluster || {},
+        { nodes = [] } = clusterOptions;
 
-      return;
-    }
+      // Delete the nodes property from options
+      delete clusterOptions.nodes;
 
-    // From this point onwards we are working with CLUSTERs
-    const clusterOptions = defaultConfig.cluster || {},
-      { nodes = [] } = clusterOptions;
+      // Add the master node to nodes list
+      nodes.unshift({
+        host: redisOptions.host,
+        port: redisOptions.port,
+      });
 
-    // Delete the nodes property from options
-    delete clusterOptions.nodes;
+      // We can now delete the host and port from redisOptions
+      delete redisOptions.host;
+      delete redisOptions.port;
 
-    // Add the master node to nodes list
-    nodes.unshift({
-      host: redisOptions.host,
-      port: redisOptions.port,
+      cache[identity] = new Redis.Cluster(nodes, {
+        ...clusterOptions,
+        redisOptions,
+      });
     });
 
-    // We can now delete the host and port from redisOptions
-    delete redisOptions.host;
-    delete redisOptions.port;
+    this.loaded = true;
+  }
+  async dispose() {
+    [...Object.values(this.$cache)].forEach((redis) => redis.disconnect());
+  }
 
-    cache[identity] = new Redis.Cluster(nodes, {
-      ...clusterOptions,
-      redisOptions,
-    });
-  });
-
-  ctx.set('cache', (name: string) => cache[name]);
-
-  return done(undefined, (next) => {
-    [...Object.values(cache)].forEach((redis) => redis.disconnect());
-    next();
-  });
+  cache(name: string) {
+    return this.$cache[name];
+  }
 }
