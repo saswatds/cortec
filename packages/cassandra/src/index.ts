70172d16ca32471bf9a94ce2d2c38e32fc6270d7
Saswat <saswatds@gmail.com>
2023-03-26 20:48:45 -0700

    feat: implement cassandra as a module

--- packages/cassandra/src/index.ts
+++ packages/cassandra/src/index.ts
@@ -1,44 +1,52 @@
-import type { Cb, Ctx, Exit } from '@cortec/types';
+import type { IConfig } from '@cortec/config';
+import type { IContext, Module } from '@cortec/types';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import sigV4 from 'aws-sigv4-auth-cassandra-plugin';
 import { Client } from 'cassandra-driver';
-import config from 'config';
 import fs from 'fs';
 
-export default function (ctx: Ctx, exit: Exit, done: Cb) {
-  const dbConfig = config.util.toObject(config.get('cassandra')),
-    db: { [name: string]: Client } = {};
+export default class CortecCassandra implements Module {
+  name = 'cassandra';
+  private clients: { [name: string]: Client } = {};
+  async load(ctx: IContext) {
+    const config = ctx.provide<IConfig>('config');
+    const dbConfig = config.get<any>('cassandra');
 
-  // eslint-disable-next-line no-restricted-syntax, guard-for-in
-  for (const identity in dbConfig) {
-    const { auth, options } = dbConfig[identity];
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const identity in dbConfig) {
+      const { auth, options } = dbConfig[identity];
 
-    let client: Client;
-    if (auth === 'aws') {
-      // Note: This will pick up credentials that have been put by vulcan
-      const auth = new sigV4.SigV4AuthProvider({
-        region: 'us-east-1',
-      });
+      let client: Client;
+      if (auth === 'aws') {
+        // Note: This will pick up credentials that have been put by vulcan
+        const auth = new sigV4.SigV4AuthProvider({
+          region: 'us-east-1',
+        });
 
-      const sslOptions = {
-        ca: [fs.readFileSync('./.cassandra/sf-class2-root.crt')],
-        host: options.contactPoints[0],
-        rejectUnauthorized: true,
-      };
-      client = new Client({ ...options, authProvider: auth, sslOptions });
-    } else {
-      client = new Client(options);
+        const sslOptions = {
+          ca: [fs.readFileSync(options.caPath)],
+          host: options.contactPoints[0],
+          rejectUnauthorized: true,
+        };
+        client = new Client({ ...options, authProvider: auth, sslOptions });
+      } else {
+        client = new Client(options);
+      }
+
+      // eslint-disable-next-line no-await-in-loop
+      await client.connect();
+
+      this.clients[identity] = client;
     }
-
-    db[identity] = client;
+  }
+  async dispose() {
+    await Promise.allSettled(
+      Object.values(this.clients).map((client) => client.shutdown())
+    );
   }
 
-  // Set the db context with a getter function
-  ctx.set('cassandra', (name: string) => db[name]);
-
-  done(undefined, (next) => {
-    [...Object.values(db)].forEach((client) => client.shutdown());
-    next();
-  });
+  client(name: string): Client | undefined {
+    return this.clients[name];
+  }
 }
