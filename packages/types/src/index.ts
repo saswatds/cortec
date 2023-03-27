abe971596e4c13bc24fe43f71068505eeaff1fad
Saswat <saswatds@gmail.com>
2023-03-26 21:31:43 -0700

    feat: remove unused types

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,16 +1,3 @@
-export type Ctx<T = any> = Map<string, T>;
-export type OnExit = (cb: (err?: string | Error) => void) => void;
-export type Exit = (code?: number) => void;
-export type Cb = (err?: string | Error, onExit?: OnExit) => void;
-
-export type IContextProvide<T = any> =
-  | {
-      (ctx: Ctx<T>, exit: Exit, done: Cb): void;
-    }
-  | {
-      (ctx: Ctx<T>, exit: Exit): Promise<OnExit | void>;
-    };
-
 export type Service = {
   name: string;
   version: string;
