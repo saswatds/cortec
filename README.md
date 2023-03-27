a45b6171829b819e75e9dfb7264fa4eb51b6a899
Saswat <saswatds@gmail.com>
2023-03-26 20:01:46 -0700

    feat: implement newrelic as a module

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 A lightweight opinionated framework for building services in Node.js
 
 ## Modules
-- [x] Load
+- [x] Core
 - [x] Config
-- [x] BullMQ
 - [x] Redis
-- [x] Cassandra
-- [x] Sentry
 - [x] MongoDB
 - [x] Newrelic
+
+- [ ] Cassandra
+- [ ] Sentry
+- [ ] BullMQ
 
   
 ### Need better api
--- packages/newrelic/src/index.ts
+++ packages/newrelic/src/index.ts
@@ -1,33 +1,30 @@
-import type { Cb, Ctx, Exit } from '@cortec/types';
+import type { IContext, Module } from '@cortec/types';
 // eslint-disable-next-line import/default
 import newrelic from 'newrelic';
 
-export default function (ctx: Ctx, exit: Exit, done: Cb) {
-  // Attach an uncaught exception handler
-  process.once('uncaughtException', (exception) => {
-    console.error(exception);
-    newrelic.noticeError(exception);
+export default class CortecNewrelic implements Module {
+  name = 'newrelic';
+  nr = newrelic;
+  async load(ctx: IContext) {
+    // Attach an uncaught exception handler
+    process.once('uncaughtException', (exception) => {
+      console.error(exception);
+      this.nr.noticeError(exception);
+      ctx.dispose(1);
+    });
 
-    // Safely shutdown new relic and exit the process
-    newrelic.shutdown({ collectPendingData: true, timeout: 3000 }, () =>
-      exit(1)
-    );
-  });
-
-  // Attach an unhandled rejection handler
-  process.once('unhandledRejection', (rejection: Error) => {
-    console.error(rejection);
-    newrelic.noticeError(rejection);
-
-    // Safely shutdown new relic and exit the process
-    newrelic.shutdown({ collectPendingData: true, timeout: 3000 }, () =>
-      exit(1)
-    );
-  });
-
-  return done(undefined, (next) => {
-    newrelic.shutdown({ collectPendingData: true, timeout: 3000 }, () =>
-      next()
-    );
-  });
+    // Attach an unhandled rejection handler
+    process.once('unhandledRejection', (rejection: Error) => {
+      console.error(rejection);
+      this.nr.noticeError(rejection);
+      ctx.dispose(1);
+    });
+  }
+  async dispose() {
+    return new Promise<void>((resolve, reject) => {
+      this.nr.shutdown({ collectPendingData: true, timeout: 3000 }, (err) =>
+        err ? reject(err) : resolve()
+      );
+    });
+  }
 }
