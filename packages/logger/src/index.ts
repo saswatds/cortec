cc084edebd401943a965c3195fd794798bb5e95b
Saswat <saswatds@gmail.com>
2023-04-06 13:33:16 -0700

    feat: add logger module

---
+++ packages/logger/src/index.ts
@@ -0,0 +1,46 @@
+import type { IConfig } from '@cortec/config';
+import type { IContext, IModule } from '@cortec/types';
+import type * as sentry from '@sentry/node';
+
+export interface ILogger {
+  silly(...args: unknown[]): void;
+  trace(...args: unknown[]): void;
+  debug(...args: unknown[]): void;
+  info(...args: unknown[]): void;
+  warn(...args: unknown[]): void;
+  error(err: Error): void;
+  fatal(err: Error): void;
+}
+
+export default class CortecLogger implements IModule, ILogger {
+  name = 'logger';
+  async load(ctx: IContext) {
+    const config = ctx.provide<IConfig>('config');
+    const loggerConfig = config?.get<sentry.NodeOptions>(this.name);
+  }
+  async dispose() {
+    /* No Op */
+  }
+
+  silly(...args: unknown[]): void {
+    /* No Op */
+  }
+  trace(...args: unknown[]): void {
+    /* No Op */
+  }
+  debug(...args: unknown[]): void {
+    /* No Op */
+  }
+  info(...args: unknown[]): void {
+    /* No Op */
+  }
+  warn(...args: unknown[]): void {
+    /* No Op */
+  }
+  error(...args: unknown[]): void {
+    /* No Op */
+  }
+  fatal(...args: unknown[]): void {
+    /* No Op */
+  }
+}
