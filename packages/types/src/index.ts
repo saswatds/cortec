5867e42ffe97832e3e33f2f27bb13e3775474a79
Saswat <saswatds@gmail.com>
2023-08-21 23:17:44 -0700

    feat: expose require on IContext

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -15,6 +15,7 @@ export interface IServerHandler {
 export interface IContext {
   service: Service;
   provide<T = unknown>(name: string): T | undefined;
+  require<T = unknown>(name: string): T;
   dispose(code: number): void;
   has(name: string): boolean;
 }
