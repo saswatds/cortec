9fc8737aec6cd28ea5f568fa9d822c5aa8ed1113
Saswat Das <saswatds@gmail.com>
2024-07-30 21:50:52 -0700

    feat: implemented workers to bullmq

--- packages/bullmq/src/index.ts
+++ packages/bullmq/src/index.ts
@@ -1,8 +1,8 @@
 import type { IConfig } from '@cortec/config';
 import type Redis from '@cortec/redis';
 import type { IContext, IModule } from '@cortec/types';
-import type { DefaultJobOptions } from 'bullmq';
-import { FlowProducer, Queue } from 'bullmq';
+import type { DefaultJobOptions, Processor } from 'bullmq';
+import { FlowProducer, Queue, Worker } from 'bullmq';
 
 interface BullConfig {
   cache: string;
@@ -19,12 +19,19 @@ interface BullMQConfig {
 export interface IBullMQ {
   queue(name: string): Queue;
   flow(name: string): FlowProducer;
+  worker(name: string): Worker;
 }
 
 export default class CortecBullMQ implements IModule, IBullMQ {
   name = 'bullMQ';
   private $queues: { [name: string]: Queue } = {};
   private $flows: { [name: string]: FlowProducer } = {};
+  private $workers: { [name: string]: Worker } = {};
+  private workerMap: Record<string, Processor>;
+
+  constructor(workerMap: Record<string, Processor>) {
+    this.workerMap = workerMap;
+  }
 
   async load(ctx: IContext) {
     const redis = ctx.require<Redis>('redis');
@@ -41,6 +48,17 @@ export default class CortecBullMQ implements IModule, IBullMQ {
     Object.entries(bullConfig?.['producer'] ?? {}).forEach(([key, val]) => {
       this.$flows[key] = new FlowProducer({
         connection: redis.cache(val.cache),
+      });
+    });
+
+    Object.entries(this.workerMap).forEach(([key, val]) => {
+      const workerConfig = bullConfig?.queue?.[key];
+      if (!workerConfig)
+        throw new Error(`No BullMQ queue config found for '${key}'`);
+
+      this.$workers[key] = new Worker(key, val, {
+        connection: redis.cache(workerConfig.cache),
+        concurrency: workerConfig.concurrency ?? 1,
       });
     });
   }
@@ -61,10 +79,19 @@ export default class CortecBullMQ implements IModule, IBullMQ {
     return q;
   }
 
+  worker(name: string) {
+    const w = this.$workers[name];
+
+    if (!w) throw new Error(`No BullMQ worker '${name}' found`);
+
+    return w;
+  }
+
   async dispose() {
     await Promise.allSettled([
       ...Object.values(this.$queues).map((q) => q.close()),
       ...Object.values(this.$flows).map((q) => q.close()),
+      ...Object.values(this.$workers).map((w) => w.close()),
     ]);
   }
 }
