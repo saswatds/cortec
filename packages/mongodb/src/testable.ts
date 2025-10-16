08b2df7e85bff9781a3dd1450bfbd2bc2451ac3b
Saswat Das <saswatds@gmail.com>
2025-10-16 15:19:22 -0700

    Add testable MongoDB module with Testcontainers support
    - Introduce TestableCortecMongodb for integration testing - Update
    config and typings for testable MongoDB usage - Add testcontainers as a
    dev dependency - Improve Redis testable module logging - Update test
    suite to use testable MongoDB and Redis - Adjust tsconfig for Node16
    module resolution

---
+++ packages/mongodb/src/testable.ts
@@ -0,0 +1,60 @@
+import type { IContext, Sig } from '@cortec/types';
+import type { StartedTestContainer } from 'testcontainers';
+import { GenericContainer } from 'testcontainers';
+
+import CortecMongodb from '.';
+
+export type TestableCortecMongodbConfig = {
+  version: string;
+};
+
+export default class TestableCortecMongodb extends CortecMongodb {
+  private testConfig: TestableCortecMongodbConfig;
+  private container?: StartedTestContainer;
+  constructor(config: TestableCortecMongodbConfig) {
+    super();
+    this.testConfig = config;
+  }
+
+  async load(context: IContext, sig: Sig) {
+    sig
+      .scope(this.name)
+      .info(
+        `starting mongodb test container with image mongo:${this.testConfig.version}...`
+      );
+    // We are going to create the test container and then override the config with the test container details
+    this.container = await new GenericContainer(
+      `mongo:${this.testConfig.version}`
+    )
+      .withExposedPorts(27017)
+      .withCommand(['mongod', '--bind_ip_all', '--replSet', 'rs0'])
+      .withHealthCheck({
+        test: [
+          'CMD-SHELL',
+          'echo \'rs.initiate({_id:"rs0",members:[{_id:0,host:"localhost:27017"}]})\' | mongo --port 27017 --quiet || true; test "$(echo \'db.isMaster().ismaster\' | mongo --port 27017 --quiet)" = "true"',
+        ],
+        interval: 5_000,
+        startPeriod: 30_000,
+      })
+      .start();
+
+    const host = this.container.getHost();
+    const port = this.container.getMappedPort(27017);
+
+    sig
+      .scope(this.name)
+      .success(`mongodb test container running on mongodb://${host}:${port}`);
+
+    // Override the config with the test container details
+    Object.entries(this.dbConfig).forEach(([_, defaultConfig]) => {
+      defaultConfig.connection.host = `${host}:${port}`;
+      defaultConfig.connection.ssl = false;
+    });
+
+    return super.load(context, sig);
+  }
+  async dispose(): Promise<void> {
+    await super.dispose();
+    await this.container?.stop();
+  }
+}
